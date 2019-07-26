52ee90866a3726705bc87d7d65b3ca39d08a5e18
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-26 14:44:31 -0700

    starting to add some variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$(document).ready(function() {}
+$(document).ready(function() {
+  trainName = $('#name').val().trim();
+ 	trainDestination = $('#destination').val().trim();
+ 	trainDeparture = $('#departure').val().trim();
+ 	trainFrequency = $('#frequency').val().trim();
+
+ 	console.log(trainName)
+ 	console.log(trainDestination)
+ 	console.log(trainDeparture)
+ 	console.log(trainFrequency)
+
+})
