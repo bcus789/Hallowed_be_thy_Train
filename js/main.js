f70445986bb8140b7a174ace2320822a3b2c4f9f
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-26 17:40:09 -0700

    slowly adding firebase

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-  trainName = $('#name').val().trim();
- 	trainDestination = $('#destination').val().trim();
- 	trainDeparture = $('#departure').val().trim();
- 	trainFrequency = $('#frequency').val().trim();
+  let trainName = $('#name').val().trim();
+ 	let trainDestination = $('#destination').val().trim();
+ 	let trainDeparture = $('#departure').val().trim();
+  let trainFrequency = $('#frequency').val().trim();
+  let minutesAway;
+  var currentTime = moment();
 
  	console.log(trainName)
  	console.log(trainDestination)
  	console.log(trainDeparture)
- 	console.log(trainFrequency)
+  console.log(trainFrequency)
+  console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyApgFiZLePsuQKK7czF8uMSgwA9o8BBhRg",
+    authDomain: "hallowed-be-thy-train.firebaseapp.com",
+    databaseURL: "https://hallowed-be-thy-train.firebaseio.com",
+    projectId: "hallowed-be-thy-train",
+    storageBucket: "",
+    messagingSenderId: "231354534972",
+    appId: "1:231354534972:web:c651acae66e4a968"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+   
 
 })
