a4dae08bacc6764a786aaceac4e2da8267630f55
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-29 19:17:13 -0700

    editing some basic ids

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
+//setting all the variables
   $("#submit").on("click", function() {
   let trainName = $('#name').val().trim();
  	let trainDestination = $('#destination').val().trim();
@@ -21,19 +22,21 @@ var database = firebase.database();
   let minutesAway;
   var currentTime = moment();
 
+//log check
  	console.log(trainName)
  	console.log(trainDestination)
  	console.log(trainDeparture)
   console.log(trainFrequency)
   console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));
 
+  //pushing object to firebase
   database.ref().push({
     name: trainName,
     destination: trainDestination,
     firstTime: trainDeparture,
     frequency: trainFrequency
 });
-  
+  // clearing inputs
 $("#name").val("");
 $("#destination").val("");
 $("#departure").val("");
@@ -42,31 +45,28 @@ $("#frequency").val("");
   return false;
 });
 database.ref().on("child_added", function(snapshot){
-	console.log(snapshot.val());
+	/* console.log(snapshot.val());
 	console.log(snapshot.val().name);
 	console.log(snapshot.val().destination);
 	console.log(snapshot.val().firstTime);
-	console.log(snapshot.val().frequency);
+	console.log(snapshot.val().frequency); */
   
-  //redefining firstTime in snapshot
-  var firstTime = snapshot.val().firstTime;
-  //redefining frequency in snapshot
-  var frequency = snapshot.val().frequency;
-  var currentTime = moment();
+
+  let trainDeparture = snapshot.val().trainDeparture;
+  let frequency = snapshot.val().frequency;
+  let currentTime = moment();
   console.log(currentTime);
 
-  var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-	  console.log(firstTimeConverted);
-  //difference in time between current time and converted time
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  let trainDepartureConverted = moment(trainDeparture, "hh:mm").subtract(1, "years");
+	  console.log(trainDepartureConverted);
+  let diffTime = moment().diff(moment(trainDepartureConverted), "minutes");
 	console.log("difference in time " + diffTime);
 
   
-  var tRemainder = diffTime % frequency;
-  var minutesAway = frequency - tRemainder;
-  var nextTime = moment().add(minutesAway, "minutes");
-  var nextTrainTime = moment(nextTime).format("hh:mm a");
+  let tRemainder = diffTime % frequency;
+  let minutesAway = frequency - tRemainder;
+  let nextTime = moment().add(minutesAway, "minutes");
+  let nextTrainTime = moment(nextTime).format("hh:mm a");
 	$("#table").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + frequency +  "</td><td>" + nextTrainTime +  "</td><td>" + minutesAway + "</td></tr>");
-	}, function(errorObject){
-  });
+	});
 })
