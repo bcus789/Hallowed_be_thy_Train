c4ec349d4e433f67118b0b3d66dbcfa7518b68d8
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-29 18:47:06 -0700

    trying to figure out why firebase isnt firing

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-/*$(document).ready(function() {})
+$(document).ready(function() {})
+
+$("#submit").on("click", function(){
   let trainName = $('#name').val().trim();
  	let trainDestination = $('#destination').val().trim();
  	let trainDeparture = $('#departure').val().trim();
   let trainFrequency = $('#frequency').val().trim();
   let minutesAway;
-  var currentTime = moment();
+  let currentTime = moment();
 
  	console.log(trainName)
  	console.log(trainDestination)
  	console.log(trainDeparture)
   console.log(trainFrequency)
-  console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));*/
+  console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));
 
   var firebaseConfig = {
     apiKey: "AIzaSyApgFiZLePsuQKK7czF8uMSgwA9o8BBhRg",
@@ -23,12 +25,53 @@
   };
   // Initialize Firebase 
   firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-  var clickCounter = 0;
-  $("#click-button").on("click", function() {
-    clickCounter++;
-    database.ref().set({
-      clickCount: clickCounter
-    });
-  });
+  var trainData = firebase.database();
+  var newTrain = {
+    name:  trainName,
+    destination: trainDestination,
+    trainTime: trainDeparture,
+    frequency: trainFrequency,
+  }
+  trainData.push(newTrain);
 
+		// clear text-boxes
+		$("#name").val("");
+		$("#destination").val("");
+		$("#departure").val("");
+    $("#frequency").val("");
+    
+    return false;
+  });
+  var trainData = firebase.database();
+  
+  trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+		console.log(childSnapshot.val());
+
+		// assign firebase variables to snapshots.
+		var firebaseName = childSnapshot.val().name;
+		var firebaseLine = childSnapshot.val().line;
+		var firebaseDestination = childSnapshot.val().destination;
+		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
+		var firebaseFrequency = childSnapshot.val().frequency;
+		
+		var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
+		var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
+		var minutes = firebaseFrequency - timeRemainder;
+
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+		
+		// Test for correct times and info
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+
+		// Append train info to table on page
+		$("#table > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseLine + "</td><td>"+ firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+
+	});
+
+
+ 
